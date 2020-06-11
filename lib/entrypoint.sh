5e5a3c1eca871a935f3b7eb23bda0e98173f53e3
Alejandro AR <abarcadabra@gmail.com>
2020-06-11 16:33:12 +0200

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gem install rubocop -v 0.80.1
-gem install rubocop-rspec -v 1.38.1
-gem install rubocop-performance -v 1.5.2
-gem install rubocop-rails -v 2.4.2
+gem install rubocop -v 0.72
+gem install rubocop-performance -v 1.4
+gem install rubocop-rails -v 2.1
+gem install rubocop-rspec -v 1.35
 
 ruby /action/lib/index.rb
